c57210d5c572ba64f816f5a1c94fa322823d5a5a
utilisateur <utilisateur@classmob-ftlv11.univ-tln.fr>
2018-11-23 16:12:08 +0100

    course

--- raimon/site/courses.php
+++ raimon/site/courses.php
@@ -8,7 +8,7 @@ error_reporting(E_ALL);
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" >
    <head>
-       <title>Cours disponibles</title>
+       <title>Client</title>
        <meta http-equiv="Content-Type" content="text/html; charset=utf8" />
        <link rel="stylesheet" media="screen" type="text/css" title="style_tab" href="css/default.css" />
    </head>
@@ -18,15 +18,19 @@ error_reporting(E_ALL);
 <?php
 include ('config.php');
 // On appelle la méthode statique get() de la classe DB qui renvoit une instance du PDO.
-$request = DB::get()->query('select * from course');
+$request = DB::get()->query('select * from client');
 ?>
 	<table>
-		<caption>Liste des cours disponibles</caption>
+		<caption>Liste des clients</caption>
 		<thead>
 			<tr>
-				<th>Code</th>
+				<th>Prénom</th>
 				<th>Nom</th>
-				<th>Description</th>
+				<th>Sexe</th>
+				<th>Adresse</th>
+				<th>CP</th>
+				<th>Ville</th>
+				<th>date dernière connexion</th>
 			</tr>
 		</thead>
 	<tbody>
@@ -35,10 +39,13 @@ $request = DB::get()->query('select * from course');
 while($data = $request->fetch()) {
 	?>
 	<tr>
-		<td><?php echo	$data['code']; ?></td> <!-- 'code' est une colonne de la BDD. -->
-		<td><?php echo	$data['name']; ?></td>
-		<td><?php echo	$data['description']; ?></td>
-	</tr>
+		<td><?php echo	$data['prenom_cli']; ?></td>
+		<td><?php echo	$data['nom_cli']; ?></td>
+		<td><?php echo	$data['sexe_cli']; ?></td>
+		<td><?php echo	$data['adresse1_cli']; ?></td>
+		<td><?php echo	$data['cp_cli']; ?></td>
+		<td><?php echo	$data['ville_cli']; ?></td>	</tr>
+		<td><?php echo	$data['dtlastconnexion_cli']; ?></td>	</tr>
 	<?php
 }
 $request->closeCursor(); // ne pas oublier de fermer le curseur.
@@ -47,8 +54,8 @@ $request->closeCursor(); // ne pas oublier de fermer le curseur.
 </table>
 
 <!-- Toutes les données du formulaire seront envoyées à la page 'insertCourse.php' avec la méthode POST. -->
-<form method="post" action="insertCourse.php">
-	<table><caption>Ajout d'un cours</caption>
+<form method="post" action="insertClient.php">
+	<table><caption>Ajout d'un client</caption>
 		<tr><td>Code : </td><td><input type="text" name="code" /></td></tr> </br>
 		<tr><td>Nom : </td><td><input type="text" name="name" /></td></tr></br>
 		<tr><td>Description : </td><td><textarea name="description" rows="5" cols="40"></textarea></tr></br> 
