96dce1c96f8b1af04a61a738292777a4e58bc0bc
David Molinari <contact@davidmolinari.fr>
2018-11-28 17:43:32 +0100

    meh

---
+++ site/dav/clientFidele.php
@@ -0,0 +1,29 @@
+<?php
+// initialisation de la session
+// INDISPENSABLE À CETTE POSITION SI UTILISATION DES VARIABLES DE SESSION.
+session_start() ;
+error_reporting(E_ALL);
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" >
+   <head>
+       <title>Tous les clients </title>
+       <meta http-equiv="Content-Type" content="text/html; charset=utf8" />
+       <link rel="stylesheet" media="screen" type="text/css" title="style_tab" href="css/default.css" />
+   </head>
+
+<body>
+
+<?php
+include ('config.php');
+// On appelle la méthode statique get() de la classe DB qui renvoit une instance du PDO.
+$request = DB::get()->query('select * from get_client_fidele();');
+?>
+<?php
+// On récupère les données. Chaque ligne est sockée dans le tableau data.
+$data = $request->fetch(); 
+$request->close();
+
+
+echo $data;
