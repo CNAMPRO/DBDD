1c4377a440f646ec4cd65364467e79930ad33e67
David Molinari <contact@davidmolinari.fr>
2018-11-29 10:32:16 +0100

    x

--- site/dav/insertCommande.php
+++ site/dav/insertCommande.php
@@ -1,14 +1,6 @@
 <html>
-<head>
-</head>
-<body>
-
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati fuga, aliquid placeat, sunt aut facilis maiores, reprehenderit minus est aperiam nulla commodi. Asperiores iure ratione ea odit voluptates sapiente voluptatum!</p>
 <?php
-
-echo 'loremlorem	lorem	lorem	lorem	lorem	lorem	lorem	lorem	lorem	lorem	lorem	lorem	lorem	lorem	lorem	lorem	lorem	lorem	lorem	lorem	lorem	lorem	lorem	lorem	lorem	lorem	lorem	lorem	lorem	lorem	lorem	lorem	lorem	lorem	lorem	v		';
+echo "PYTAIN";
 print_r($_POST['meh']);
 ?>	
-</body>
-</html>
 
